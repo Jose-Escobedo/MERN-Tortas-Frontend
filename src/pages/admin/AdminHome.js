36a8271a780df8ab113d299b30754c7d69661e6e
+ <escobedo.jose48@outlook.com>
2023-07-15 18:44:37 -0700

    editing https req

--- src/pages/admin/AdminHome.js
+++ src/pages/admin/AdminHome.js
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 import { io } from "socket.io-client";
 import sound from "./Assets/smb_powerup.wav";
 import AdminModal from "../../components/AdminModal";
-const socket = io.connect("http://tortasbackend.herokuapp.com");
+const socket = io.connect("https://tortasbackend.herokuapp.com");
 
 const AdminHome = () => {
   const user = useSelector((state) => state.user.currentUser);
