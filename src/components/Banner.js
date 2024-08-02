64276c3d9585f000d457555c2d100382e929b269
jose <escobedo.jose48@outlook.com>
2024-08-01 23:04:37 -0700

    editing banner

---
+++ src/components/Banner.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Banner = () => {
+  return (
+    <>
+      <BannerContainer>
+        <BannerContainerText>
+          Due to ongoing improvements to our delivery system, we are temporarily unable to accept online delivery orders. We apologize for any inconvenience and appreciate your understanding as we work to enhance our service. We look forward to resuming deliveries soon.
+        </BannerContainerText>
+      </BannerContainer>
+    </>
+  );
+}
+
+const BannerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 20px;
+  background-color: cyan;
+  height: auto;
+
+  @media (max-width: 768px) {
+    padding: 15px;
+  }
+`;
+
+const BannerContainerText = styled.h2`
+  font-weight: 300;
+  margin: 0;
+  font-family: Urbanist, sans-serif;
+  font-size: 1.5rem;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+export default Banner;
